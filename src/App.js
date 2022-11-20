99ed07b0374f41011540f7dbbb372362a97a2ebb
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-11-20 20:01:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./components/Header";
+import StoryList from "./components/StoryList";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Header />
+                <Routes>
+                    <Route path="/:type" element={<StoryList />} />
+                    <Route index element={<Navigate to="/top" />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+};
 
 export default App;
